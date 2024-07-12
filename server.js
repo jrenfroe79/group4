3f36a15891c94dcf777be9454c8d9b4c1def2d41
jrenfroe79 <157231633+jrenfroe79@users.noreply.github.com>
2024-07-12 14:31:26 -0400

    updated files quotes

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require(`express`);
+const morgan = require(`morgan`);
+const mongoose = require(`mongoose`);
+const path = require(`path`);
 
 // Create the Express app
 const app = express();
 
 // Middleware for serving static files under '/styles' route
-app.use('/styles', express.static(path.join(__dirname, 'public')));
+app.use(`/styles`, express.static(path.join(__dirname, `public`)));
 
 // Set the static directory for serving files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, `public`)));
 
 // Middleware for logging
-app.use(morgan('dev'));
+app.use(morgan(`dev`));
 
 // Handle requests for favicon.ico
-app.get('/favicon.ico', (req, res) => {
+app.get(`favicon.ico`, (req, res) => {
   res.status(204).end();
 });
 
 // Define routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get(`/`, (req, res) => {
+  res.sendFile(path.join(__dirname, `public`, `index.html`));
 });
 
 // Start the server on a dynamically assigned port
