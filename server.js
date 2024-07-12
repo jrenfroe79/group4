8620304d63b78fe13b5cd5edb608e04566539d70
jrenfroe79 <157231633+jrenfroe79@users.noreply.github.com>
2024-07-12 07:28:27 -0400

    update server.js file

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
-app.use(express.static('public')); // Add this line to serve static files
+
 
 const dbURI = "mongodb+srv://group4:jjrgroup4@group4.jbaccmx.mongodb.net/?retryWrites=true&w=majority&appName=group4"
 
@@ -17,6 +15,14 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const Course = require('./models/course');
 const Teacher = require('./models/teacher');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
 // Define routes for courses and teachers
 
