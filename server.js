dffd8880a7011bea9d9b0d1a0bd1b40d34eab9ef
jrenfroe79 <157231633+jrenfroe79@users.noreply.github.com>
2024-07-12 08:51:08 -0400

    moved html and css in one public folder and updated to a default port as available

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 3000;
 
+const dbURI = "mongodb+srv://group4:jjrgroup4@group4.jbaccmx.mongodb.net/?retryWrites=true&w=majority&appName=group4";
 
-
-const dbURI = "mongodb+srv://group4:jjrgroup4@group4.jbaccmx.mongodb.net/?retryWrites=true&w=majority&appName=group4"
-
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
+mongoose.connect(dbURI)
+  .then(result => {
+    const server = app.listen(0, () => {
+      const PORT = server.address().port;
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
   .catch(err => console.log(err));
 
 const Course = require('./models/course');
 const Teacher = require('./models/teacher');
 
-// middleware & static files
+// Middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
@@ -25,19 +27,18 @@ app.use((req, res, next) => {
 });
 
 // Define routes for courses and teachers
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.get('/courses', (req, res) => {
-    Course.find({}, (err, courses) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.json(courses);
-        }
-    });
+  Course.find({}, (err, courses) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(courses);
+    }
+  });
 });
 
 // Add routes for creating, updating, and deleting courses and teachers
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
